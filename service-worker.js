f2ab315d2bfcf25ed5e30eff3d1d14639086cb9d
polishcrewmobiledetail <polishcrewmobiledetail@gmail.com>
2025-10-21 19:25:15 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'polishcrew-cache-v8';
+const CACHE_NAME = 'polishcrew-cache-v9';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./logo-header.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request).then(r => {
-    const copy = r.clone(); caches.open(CACHE_NAME).then(c => c.put(e.request, copy)); return r;
+    const copy=r.clone(); caches.open(CACHE_NAME).then(c => c.put(e.request, copy)); return r;
   }).catch(() => caches.match('./index.html'))));
 });
